13865c2035aabdee97fac015b93f664cfd5f56d1
unknown <komarnicki115@gmail.com>
2018-01-13 15:02:30 +0100

    add friend modal

--- views/src/main-control.js
+++ views/src/main-control.js
@@ -16,8 +16,26 @@ $(() => {
 		$(".ui.logs.modal")
 		.modal("show");
 	})
+
+	$(document).on("click", ".sidebar#friends-list .add-friend-btn", (e) => {
+		$(".ui.add-friend.modal")
+		.modal({
+			onApprove() {
+				const friend = {
+					name: $(".ui.add-friend.modal input#friend-name").val(),
+					apiKey: $(".ui.add-friend.modal input#friend-api").val()
+				}
+				addFriend(friend);
+			}
+		})
+		.modal("show");
+	})
 })
 
 function saveSettings (settings) {
 	console.log("new settings ", settings)
+}
+
+function addFriend (user) {
+	console.log("new friend ", user)
 }
